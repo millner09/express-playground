ed3823777ef1029d9ae13bf07ffac501dbc17525
Adam Millner <adam.millner@gmail.com>
2024-05-18 21:55:59 -0500

    Add beforeEach logic to userService.test.js

--- tests/services/userService.test.js
+++ tests/services/userService.test.js
@@ -1,45 +1,60 @@
-import { jest } from '@jest/globals';
+import { jest } from "@jest/globals";
 
-jest.unstable_mockModule('../../db/index.js', () => ({
-    query: jest.fn(),
-    // etc.
+jest.unstable_mockModule("../../db/index.js", () => ({
+  query: jest.fn(),
+  // etc.
 }));
 
-const { query } = await import('../../db/index.js');
-const { getAllUsers, getUserById, createUser } = await import('../../services/userService.js');
+const { query } = await import("../../db/index.js");
+const { getAllUsers, getUserById, createUser } = await import(
+  "../../services/userService.js"
+);
 
-test('getAllUsers should fetch users', async () => {
-    const users = [{ name: 'Adam Millner', email: "adam.millner@gmail.com" }];
+test("getAllUsers should fetch users", async () => {
+  const users = [{ name: "Adam Millner", email: "adam.millner@gmail.com" }];
 
-    query.mockResolvedValue({ rows: users });
+  query.mockResolvedValue({ rows: users });
 
-    // or you could use the following depending on your use case:
-    // axios.get.mockImplementation(() => Promise.resolve(resp))
-    const getAllUsersResult = await getAllUsers();
-    return expect(getAllUsersResult).toEqual(users);
+  // or you could use the following depending on your use case:
+  // axios.get.mockImplementation(() => Promise.resolve(resp))
+  const getAllUsersResult = await getAllUsers();
+  return expect(getAllUsersResult).toEqual(users);
 });
 
-test('getUserById should fetch user', async () => {
-    const users = [{ id: 1, name: 'Adam Millner', email: 'adam.millner@gmail.com' }]
-    query.mockResolvedValue({ rows: users })
-
-    const getUserByIdResult = await getUserById(users[0].id);
-    return expect(getUserByIdResult).toEqual(users[0])
+beforeEach(() => {
+  query.mockClear();
 });
 
-test('getUserById should return undefined when no users are found', async () => {
-    const users = [];
-    query.mockResolvedValue({ rows: users })
+test("getUserById should fetch user", async () => {
+  const users = [
+    { id: 1, name: "Adam Millner", email: "adam.millner@gmail.com" },
+  ];
+  query.mockResolvedValue({ rows: users });
 
-    const getUserByIdResult = await getUserById(1);
-    return expect(getUserByIdResult).toEqual(undefined)
+  const getUserByIdResult = await getUserById(users[0].id);
+  return expect(getUserByIdResult).toEqual(users[0]);
 });
 
-test('createUser should return the created user when they are successfully created', async () => {
-    const newUser = { id: 1, name: "Adam Millner", email: "adam.millner@gmail.com" }
+test("getUserById should return undefined when no users are found", async () => {
+  const users = [];
+  query.mockResolvedValue({ rows: users });
 
-    query.mockResolvedValue({ rows: [newUser] })
+  const getUserByIdResult = await getUserById(1);
+  return expect(getUserByIdResult).toEqual(undefined);
+});
 
-    const createUserResult = await createUser({ name: newUser.name, email: newUser.email });
-    return expect(createUserResult).toEqual(newUser)
-})
+test("createUser should return the created user when they are successfully created", async () => {
+  const newUser = {
+    id: 1,
+    name: "Adam Millner",
+    email: "adam.millner@gmail.com",
+  };
+
+  query.mockResolvedValue({ rows: [newUser] });
+
+  const createUserResult = await createUser({
+    name: newUser.name,
+    email: newUser.email,
+  });
+  return expect(createUserResult).toEqual(newUser);
+});
